f2801a06162e4e645de85d7acf7b3f758782bbd2
Dane Wilson <dnkw@hotmail.com>
2023-09-08 04:04:26 -0500

    Apply styling
    Signed-off-by: Dane Wilson <dnkw@hotmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import './App.scss'
 import { OfferList } from './offers/Offers'
-
+import { Nav } from './shared/Nav'
 
 const App = () => {
   return (
     <>
-      <OfferList />
+      <Nav />
+      <main className='main'>
+        <div className="container">
+          <OfferList />
+        </div>
+      </main>
     </>
   )
 }
---
+++ client/src/shared/Nav.jsx
@@ -0,0 +1,27 @@
+import logo from '../assets/king-icon.png'
+import './Nav.scss'
+
+const Logo = ({ className }) => {
+  return (
+    <a href='/' className={`logo ${className}`}>
+      <div className='logo-img'>
+        <img src={logo} className='img' />
+      </div>
+      <span>OfferKing</span>
+    </a>
+  )
+}
+
+const Nav = () => {
+  return(
+    <nav className='navbar'>
+      <div className='container'>
+        <div className="nav-content">
+          <Logo className='brand' />
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export { Nav, Logo }
